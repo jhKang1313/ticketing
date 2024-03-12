94591483a1296a6127cd659f3877e53a909aba04
jhkang1313 <jhkang1313@gmail.com>
2024-03-12 23:13:57 +0900

    토트넘 좌석 정보 조회 API 에 호출하여 결과값받기
    결과 성공이면 webhook 이용해서 결과 전송

--- Message.py
+++ Message.py
@@ -1,21 +1,18 @@
 from slack_sdk import WebhookClient
 from slack_sdk.errors import SlackApiError
-from datetime import datetime
-def send_slack_notification(webhook_url, message):
-    client = WebhookClient(webhook_url)
+def send_slack_notification(message):
+
+    client = WebhookClient("https://hooks.slack.com/services/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")
 
     try:
         response = client.send(text=message)
     except SlackApiError as e:
         print("Error sending Slack notification:", e.response["error"])
 
-slack_webhook_url = "https://hooks.slack.com/services/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"
-
-datetime.strftime(datetime.now(), 'yyyyMMdd')
-notification_message = f"Time is {datetime.strftime(datetime.now(), 'yyyyMMdd')}"
+notification_message = "이제또 까네"
 
 # Slack에 알림 보내기
-send_slack_notification(slack_webhook_url, notification_message)
+send_slack_notification(notification_message)
 
 
 
