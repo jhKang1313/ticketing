0ad992842787fe130776c1cbc6970fd31aace4d3
jhkang1313 <jhkang1313@gmail.com>
2024-03-13 23:31:24 +0900

    Message 테스트 코드 주석처리 및 Header 부분 필수값 주석 추가

--- Message.py
+++ Message.py
@@ -1,18 +1,16 @@
 from slack_sdk import WebhookClient
 from slack_sdk.errors import SlackApiError
 def send_slack_notification(message):
-
     client = WebhookClient("https://hooks.slack.com/services/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")
-
     try:
         response = client.send(text=message)
     except SlackApiError as e:
         print("Error sending Slack notification:", e.response["error"])
 
-notification_message = "이제또 까네"
+#notification_message = "이제또 까네"
 
 # Slack에 알림 보내기
-send_slack_notification(notification_message)
+#send_slack_notification(notification_message)
 
 
 
